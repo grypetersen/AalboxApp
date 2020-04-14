b62aef3d0d9eb35fcfadeff901ab37e3aafdcc04
Patrick Alminde <palminde@hotmail.com>
2020-04-14 18:09:06 +0200

    Added functionality to addpostactivity

--- app/src/main/java/com/example/aalboxapp/AddPostActivity.java
+++ app/src/main/java/com/example/aalboxapp/AddPostActivity.java
@@ -2,13 +2,29 @@ package com.example.aalboxapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class AddPostActivity extends AppCompatActivity {
+    private PostViewModel postViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_post);
+    }
+
+    public void addPostToFeed(View v){
+        EditText location = (EditText)findViewById(R.id.text_view_insert_location);
+        EditText category = (EditText)findViewById(R.id.text_view_insert_category);
+        EditText description = (EditText)findViewById(R.id.text_view_insert_description);
+
+        Post post = new Post(String.valueOf(location.getText()), String.valueOf(category.getText()), String.valueOf(description.getText()),0,0,"123");
+        postViewModel.insert(post);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
